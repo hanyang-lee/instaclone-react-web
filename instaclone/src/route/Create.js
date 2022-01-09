3577965d67e2ab4f29b3034ee25b8f4ca3e2c2d0
matjji <pon03046@naver.com>
2022-01-09 15:01:32 +0900

    Create input 및 iconinverted 수정

--- instaclone/src/route/Create.js
+++ instaclone/src/route/Create.js
@@ -51,7 +51,7 @@ const Create = () =>{
                     <h2 className={styles.subtitle}>친구들의 사진과 동영상을 보려면 가입하세요.</h2>
                     <div>
                         <button className={styles.facebutton} onClick={onGoogleClick}>
-                            <span className={classNames({[styles.coreSpriteFacebookIcon]: true, [styles.picture]: true})}></span>
+                            <span className={classNames({[styles.coreSpriteFacebookIconInverted]: true, [styles.picture]: true})}></span>
                             <span className={styles.facetitle}>Facebook으로 로그인</span>
                         </button>
                     </div>
